c1e5fa7a743e613d58fac91d2b6bec8b5a66ab94
tosi29 <tosi16+github@gmail.com>
2017-06-27 00:12:00 +0900

    Atcoder beginner065 (2017/06/24)

---
+++ 20170624_beginner065/b/Main.java
@@ -0,0 +1,53 @@
+import java.util.BitSet;
+import java.util.Scanner;
+
+
+public class Main {
+	
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String line = sc.nextLine();
+
+        
+        int n = Integer.parseInt(line);
+        
+        int a[] = new int[n+1];
+        
+        for (int i = 1; i <= n; i++) {
+        	line = sc.nextLine();
+			a[i] = Integer.parseInt(line);
+		}
+        
+        BitSet bits = new BitSet();
+        
+        int current = 1;	// Button 1 on
+        bits.set(1);	// button 1 on
+        int count = 0;
+
+        while (true) {
+        	count++;
+        	int next = a[current];
+        	
+        	if (next == 2) {
+        		System.out.println(count);
+        		return;
+        	}
+        	
+        	if (bits.get(next)) {
+        		System.out.println(-1);
+        		return;
+        	} else {
+        		bits.set(next);
+        	}
+        	
+        	current = next;
+        }
+        
+        
+        
+	}
+	
+
+	
+
+}
