e45c6c1ea8c296279f8f5aa7154386053cb68797
tosi29 <tosi16@gmail.com>
2021-03-28 11:42:38 +0900

    Polish

--- src/beginner197/c/Main.py
+++ src/beginner197/c/Main.py
@@ -1,22 +1,21 @@
 N = int(input())
 A = list(map(int, input().split()))
 
-def SPLIT(list, n):
-    result = 0
+def search(list, n):
+    xor_result = 0
     or_result = 0
     for i in range(N):
         or_result |= list[i]
         if n & (1 << i):
-            result ^= or_result
+            xor_result ^= or_result
             or_result = 0
-    result ^= or_result
-    return result
+    xor_result ^= or_result
+    return xor_result
 
 min = 2 ** 30
 for i in range(2**(N-1)):
-    result = SPLIT(A,i)
+    result = search(A, i)
     if result < min:
         min = result
 
 print(min)
-
