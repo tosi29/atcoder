36c2e752e64e4545d5a6c2b67d62137cfd89dfd6
tosi29 <tosi16@gmail.com>
2021-05-15 22:25:25 +0900

    Add python cheat sheet

---
+++ src/atcoder_common/python.py
@@ -0,0 +1,28 @@
+# int input
+N = int(input())
+
+# List input
+A = list(map(int, input().split()))
+
+# Multiple int input
+H, W = map(int, input().split())
+
+# String array input
+A = [input() for i in range(H)]
+
+# Generate Permutation
+import itertools
+itertools.permutations('0123456789', 10)
+
+# Ceil and Floor
+import math
+math.ceil(N)
+math.floor(N)
+
+# Divide as integer
+import math
+math.floor(N/N)
+
+# Square root
+import math
+math.sqrt(N)
