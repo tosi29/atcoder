9ae874e448cd46d12c034f353d05538a53500041
tosi29 <tosi16@gmail.com>
2021-05-31 01:34:46 +0900

    Atcoder Beginner 203 D

---
+++ src/beginner203/d/Main.py
@@ -0,0 +1,42 @@
+N, K = map(int, input().split())
+A=[list(map(int, input().split())) for i in range(N)]
+ 
+left = -1
+right = 1000000001
+ 
+t = K**2//2+1
+ 
+while (left+1) < right:
+    search = (left + right) // 2
+ 
+    s = [[0]*(N+1) for i in range(N+1)]
+    for i in range(N):
+        for j in range(N):
+            if A[i][j] > search:
+                s[i+1][j+1] = 1
+            else:
+                s[i+1][j+1] = 0
+ 
+    # right
+    for i in range(N+1):
+        for j in range(N):
+           s[i][j+1] += s[i][j]
+ 
+    # down
+    for i in range(N):
+        for j in range(N+1):
+            s[i+1][j] += s[i][j]
+ 
+    # judge
+    flag = False
+    for i in range(N-K+1):
+        for j in range(N-K+1):
+            target = s[i][j] - s[i+K][j] - s[i][j+K] + s[i+K][j+K]
+            if target < t:
+                flag = True
+    if flag:
+        right = search
+    else:
+        left = search
+ 
+print(right)
